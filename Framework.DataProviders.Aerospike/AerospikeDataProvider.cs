e02871c49f53383eebd53370e41539c0389b8cc2
ealeksandrovich <ealeksandrovich@productiveedge.com>
2016-11-06 19:38:43 +0300

    Aerospike data provider added

---
+++ Framework.DataProviders.Aerospike/AerospikeDataProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Framework.DataProviders.Aerospike
+{
+    using global::Aerospike.Client;
+
+    public class AerospikeDataProvider
+    {
+        public const int DefaultPort = 3000;
+
+        public AerospikeClient Client { get; }
+
+        public AerospikeDataProvider(string hostName) : this(hostName, DefaultPort)
+        {
+        }
+
+
+        /// <summary>
+        /// http://www.aerospike.com/docs/client/csharp/usage/connect_sync.html
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="port"></param>
+        public AerospikeDataProvider(string hostName, int port)
+        {
+            Client = new AerospikeClient(hostName, port);
+        }
+
+        public void Close()
+        {
+            Client.Close();
+        }
+    }
+}
---
+++ Framework.Tests/DataProviders/AerospikeTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Framework.Tests.DataProviders
+{
+    using System;
+    using Aerospike.Client;
+    using Framework.DataProviders.Aerospike;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AerospikeTests
+    {
+        private const string Namespace = "AerospikeNs";
+        private const string SetName = "AerospikeSet";
+        private const string KeyName = "AerospikeUsers";
+
+        private readonly AerospikeDataProvider provider;
+
+        public AerospikeTests()
+        {
+            this.provider = new AerospikeDataProvider("52.36.99.52", 3000);
+        }
+
+        /// <summary>
+        /// http://www.aerospike.com/docs/client/csharp/usage/kvs/write.html
+        /// </summary>
+        [Test]
+        public void PutTest()
+        {
+            var user = new AerospikeUser
+            {
+                UserId = new Guid("6B16F333-0B46-4057-9FA6-7CF630A0A0BC"),
+                Name = "AerospikeUser"
+            };
+
+            var key = new Key(Namespace, SetName, KeyName);
+
+            var bin = new Bin(user.UserId.ToString(), user);
+
+            this.provider.Client.Put(null, key, bin);
+        }
+
+        /// <summary>
+        /// http://www.aerospike.com/docs/client/csharp/usage/kvs/read.html
+        /// </summary>
+        [Test]
+        public void GetTest()
+        {
+            var key = new Key(Namespace, SetName, KeyName);
+
+            Record record = this.provider.Client.Get(null, key, "6B16F333-0B46-4057-9FA6-7CF630A0A0BC");
+
+            Assert.IsNotNull(record);
+            var user = record.GetValue("6B16F333-0B46-4057-9FA6-7CF630A0A0BC") as AerospikeUser;
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user.Name, "AerospikeUser");
+        }
+    }
+
+    public class AerospikeUser
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
